efdb22e08fba3c5ecac022e64ecdb9528a875abb
gabriel <galhiardogabriel@gmail.com>
2025-05-16 12:00:28 -0300

    Perf: Otimiza o código para melhorar o desempenho

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var vida = 100;
+var vidaAtualizada = 100;
 var pedras = 0;
 var bonusPedra = 50;
 var dano = 1;
@@ -15,9 +16,7 @@ function minerar(){
     let picareta = document.getElementById("picareta");
     let pedra = document.getElementById("pedra");
     let botao = document.getElementById("botaoMinerar");
-    let textoVida = document.getElementById("vidaTotal");
-    let statusVida = document.getElementById("vidaPerdida");
-    let textoPedra = document.getElementById("statusPedra");
+
 
     //animação da picareta
     picareta.style.animation = 'none';
@@ -32,11 +31,12 @@ function minerar(){
   }, 1000);
 
     //tirando vida da pedra
-    vida = vida - dano;
+    vidaAtualizada = vidaAtualizada - dano;
     
     //ganhando pedras
-    if(vida <= 0){
-        vida = 100;
+    if(vidaAtualizada <= 0){
+        vidaAtualizada = vida;
+
         // Ganho matatando a Pedra
         pedras += Math.floor(bonusPedra * multiplicadorDaPicareta);
 
@@ -46,86 +46,86 @@ function minerar(){
     }else{
         // Ganho sem matar a Pedra
         pedras += Math.floor(dano * multiplicadorDaPicareta); 
-
         pedra.style.animation = 'none';
         void pedra.offsetWidth;
         pedra.style.animation = `pedraHit 0.7s linear 1 0.4s`;      
     }
-    textoVida.innerText = `${vida}Hp`;
-
-    //mudando o texto da pedra
-    textoPedra.innerText = `Pedras: ${pedras}`;
-    
-    //mudando a barrinha de vida
-    statusVida.style.width = `${vida}%`;
-
 
 }
 
 // Upgrades
 
 function melhoriaDano(){
-    let textoDanoUpgrade = document.getElementById("valorDanoUpgrade");
-    let statusDano = document.getElementById("statusDano");
-    let statusPedra = document.getElementById("statusPedra");
+
     if(pedras >= valorUpgradeDano){
             pedras = pedras - valorUpgradeDano;
             dano = dano + 1;
             nivelDano += 1;
             valorUpgradeDano = Math.floor(20 * (1.5 ** (nivelDano - 1)));
-            
-            textoDanoUpgrade.innerText = `Custo: ${valorUpgradeDano} Pedras`;
-            statusDano.innerText = `Dano: ${dano}`;
-            statusPedra.innerText = `Pedras: ${pedras}`;   
 
     }
+
 }
 
 function melhoriaPicareta(){
-    let textoEvolucaoUpgrade = document.getElementById("valorEvoluirUpgrade");
-    let statusDano = document.getElementById("statusDano");
-    let statusPicareta = document.getElementById("statusPicareta");
-    let textoDanoUpgrade = document.getElementById("valorDanoUpgrade");
 
     if(dano >= valorEvoluirUpgrade){
         dano = 1;
         nivelDano = 1;
         statusDano.innerText = `Dano: ${dano}`;
 
-
         multiplicadorDaPicareta = multiplicadorDaPicareta * 1.4;
         valorEvoluirUpgrade = Math.floor(valorEvoluirUpgrade * 1.64);
         valorUpgradeDano = Math.floor(20 * (1.5 ** (nivelDano - 1)));
-
-        textoDanoUpgrade.innerText = `Custo: ${valorUpgradeDano} Pedras`;
-        textoEvolucaoUpgrade.innerText = `Custo: ${valorEvoluirUpgrade} Dano`;
-        statusPicareta.innerText = `Multiplicador da Picareta: ${multiplicadorDaPicareta}`;
     }
+
 }
 
 function melhoriaPedra(){
-    let textoPedraUpgrade = document.getElementById("valorPedraUpgrade");
-    let statusPedra = document.getElementById("statusPedra");
-    let statusBoostPedra = document.getElementById("statusBoostPedra");
-    let textoVida = document.getElementById("vidaTotal");
 
 
     if(pedras >= valorPedraUpgrade){
         pedras = pedras - valorPedraUpgrade;
         vida = Math.floor(vida * 1.323);
+        vidaAtualizada = vida;
         valorPedraUpgrade = Math.floor(valorPedraUpgrade * 1.61);
 
-
         bonusPedra = Math.floor(bonusPedra * 2.2);
-        statusBoostPedra.innerText = `Pedra boost final: ${bonusPedra} pedras`;
-            
-        textoPedraUpgrade.innerText = `Custo: ${valorPedraUpgrade} Pedras`; 
-        statusPedra.innerText = `Pedras: ${pedras}`;
-        textoVida.innerText = `${vida}Hp`;
-
     }
 }
 
+
+function atualizarStatus(){
+    let statusDano = document.getElementById("statusDano");
+    let textoDanoUpgrade = document.getElementById("valorDanoUpgrade");
+
+    textoDanoUpgrade.innerText = `Custo: ${valorUpgradeDano} Pedras`;
+    statusDano.innerText = `Dano: ${dano}`;
+
+    
+    let statusPicareta = document.getElementById("statusPicareta");
+    let textoEvolucaoUpgrade = document.getElementById("valorEvoluirUpgrade");
+    statusPicareta.innerText = `Multiplicador da Picareta: ${multiplicadorDaPicareta}`;
+    textoEvolucaoUpgrade.innerText = `Custo: ${valorEvoluirUpgrade} Dano`;
+    
+
+    let statusPedra = document.getElementById("statusPedra");
+    let textoPedraUpgrade = document.getElementById("valorPedraUpgrade");
+    let statusBoostPedra = document.getElementById("statusBoostPedra");
+
+    statusPedra.innerText = `Pedras: ${pedras}`;
+    textoPedraUpgrade.innerText = `Custo: ${valorPedraUpgrade} Pedras`; 
+    statusBoostPedra.innerText = `Pedra boost final: ${bonusPedra} pedras`;
+    
+    let statusVida = document.getElementById("vidaPerdida");
+    let textoVidaUpgrade = document.getElementById("vidaTotal");
+
+    statusVida.style.width = `${vidaAtualizada}%`;
+    textoVidaUpgrade.innerText = `${vidaAtualizada}Hp`;
+
+}
+
+setInterval(atualizarStatus, 1000);
 
 // Alteração de tela
 
